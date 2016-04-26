865a63c538cb5e7bdec790790cc1767dccf6b2a8
Ajith ER <ajither@merclain.com>
2016-04-26 19:40:15 +0530

    First commit

---
+++ AndroidTabActvity/TabActivity/app/src/main/java/com/codedetective/tabactivity/Fragment/Three.java
@@ -0,0 +1,28 @@
+package com.codedetective.tabactivity.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codedetective.tabactivity.R;
+
+public class Three extends Fragment {
+    public Three() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+
+}
